2efff1491a0b68b5e190ce68370edc38c68936c8
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 01:36:13 +0300

    chore: refresh map loader

--- maps.js
+++ maps.js
@@ -1,1 +1,1 @@
-(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830';s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-mapfix';s.defer=true;document.head.appendChild(s)})();
