8fafeda2e68da74b8081f26cb9f0ebacab88c736
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 02:25:40 +0300

    Load compact white map core

--- maps.js
+++ maps.js
@@ -1,1 +1,1 @@
-(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-three-tab-hub';s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-compact-white-map';s.defer=true;document.head.appendChild(s)})();
