495bf25b6e106164afd2fcd3b48d7b73fd553c0f
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 16:19:09 +0300

    3D ikon hızlandırma: maps-core.js

--- maps-core.js
+++ maps-core.js
@@ -214,9 +214,4 @@
   pharmacy.defer=true;
   pharmacy.dataset.elmaPharmacyService='1';
   document.head.appendChild(pharmacy);
-  const iconSkin=document.createElement('script');
-  iconSkin.src='icon-skin-3d.js?v=20260830-svg-nav';
-  iconSkin.defer=true;
-  iconSkin.dataset.elma3dIconSkin='1';
-  document.head.appendChild(iconSkin);
 })();
