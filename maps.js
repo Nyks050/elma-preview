ed70cc2fc5d0a9ae125a18ff23d38cfa090175f2
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 12:53:56 +0300

    Refresh maps core

--- maps.js
+++ maps.js
@@ -1,1 +1,1 @@
-(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-lines12';s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-frequency-labels';s.defer=true;document.head.appendChild(s)})();
