217ee4fbb3546483336b23ba57090d63030b7d44
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 14:53:06 +0300

    Refresh bundle for permission label

--- maps.js
+++ maps.js
@@ -1,1 +1,1 @@
-(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-pharmacy-auto-location';s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-pharmacy-permission';s.defer=true;document.head.appendChild(s)})();
