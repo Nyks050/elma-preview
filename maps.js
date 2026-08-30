a389da708f4a4a5fc62d6b5c05c531c08f749c0a
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-08-30 14:39:35 +0300

    Add location-based on-duty pharmacy service (#27)
    Connect the Services screen to the secured Cloudflare Worker and show nearby on-duty pharmacies on demand.

--- maps.js
+++ maps.js
@@ -1,1 +1,1 @@
-(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-line6';s.defer=true;document.head.appendChild(s)})();
+(()=>{let s=document.createElement('script');s.src='maps-core.js?v=20260830-pharmacy';s.defer=true;document.head.appendChild(s)})();
