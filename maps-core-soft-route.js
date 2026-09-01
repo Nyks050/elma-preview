4e4e2e4131ebd7f00d610bf389f4423159e16ebc
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-09-01 15:30:35 +0300

    Make journey results compact and draggable

--- maps-core-soft-route.js
+++ maps-core-soft-route.js
@@ -796,7 +796,7 @@ body:not(.elma-white-flow) #elmaHomeWidgets:not(.home-active){display:block!impo
   line1Planner.dataset.elmaLine1Planner='1';
   document.head.appendChild(line1Planner);
   const journeyResults=document.createElement('script');
-  journeyResults.src='journey-results.js?v=20260901-route-stable';
+  journeyResults.src='journey-results.js?v=20260901-compact-sheet1';
   journeyResults.defer=true;
   journeyResults.dataset.elmaJourneyResults='1';
   document.head.appendChild(journeyResults);
