6fd1af0525ea36d3a8e4ee50d61cccd5d6ef4926
Nyks <101414702+Nyks050@users.noreply.github.com>
2026-09-07 23:27:35 +0300

    Yakındaki Duraklar hizmetine ikon ekle (#62)
    * Add prominent icon to nearby stops service
    
    * Refresh nearby stops icon cache
    
    * Load nearby stops icon update
    
    * Load nearby stops card icon
    
    * Load nearby stops icon in current preview

--- maps-core-soft-route.js
+++ maps-core-soft-route.js
@@ -821,7 +821,7 @@ body:not(.elma-white-flow) #elmaHomeWidgets:not(.home-active){display:block!impo
   line6Route.dataset.elmaLine6Route='1';
   document.head.appendChild(line6Route);
   const nearbyStops=document.createElement('script');
-  nearbyStops.src='nearby-stops-service.js?v=20260907-001';
+  nearbyStops.src='nearby-stops-service.js?v=20260907-icon';
   nearbyStops.defer=true;
   nearbyStops.dataset.elmaNearbyStops='1';
   document.head.appendChild(nearbyStops);
